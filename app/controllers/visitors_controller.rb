bfeef0e3c563b188c668d8ba5e6b0794f09d831c
zero2smart <fivewisdomzero@gmail.com>
2018-12-06 10:15:47 -0500

    test

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,7 @@
 class VisitorsController < ApplicationController
     helper BookHelper
     def index
-        @times = helpers.book_time
+        # @times = helpers.book_time
+        @times = []
     end
 end
