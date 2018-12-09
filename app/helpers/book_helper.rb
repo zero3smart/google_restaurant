423e7bc39a64cc936744d3b6258c23ff48b41bc4
zero2smart <fivewisdomzero@gmail.com>
2018-12-09 15:17:43 -0500

    test

--- app/helpers/book_helper.rb
+++ app/helpers/book_helper.rb
@@ -8,7 +8,7 @@ module BookHelper
         options = Selenium::WebDriver::Chrome::Options.new
         options.add_argument('--headless')
         Selenium::WebDriver::Chrome.driver_path=Dir.pwd + "/bin/chromedriver"
-        driver = Selenium::WebDriver.for :chrome, options: options
+        driver = Selenium::WebDriver.for :chrome#, options: options
 
         # driver.get "https://www.google.com/maps"
         driver.get "https://www.google.com/maps/reserve/v/dine/c/TxwxStfp_yo?source=pa&hl=en-US&gei=tzoIXMzjHNLS8wXyoYuoDQ&sourceurl=https://www.google.com/maps/preview/entity?authuser%3D0%26hl%3Den%26gl%3Dus%26pb%3D!1m14!1s0x89e3889885a26111:0xa7767b093a1ab17c!3m12!1m3!1d2640.8196502614906!2d-76.99751528255612!3d38.880642099999996!2m3!1f0!2f0!3f0!3m2!1i510!2i653!4f13.1!12m3!2m2!1i392!2i106!13m57!2m2!1i203!2i100!3m2!2i4!5b1!6m6!1m2!1i86!2i86!1m2!1i408!2i200!7m42!1m3!1e1!2b0!3e3!1m3!1e2!2b1!3e2!1m3!1e2!2b0!3e3!1m3!1e3!2b0!3e3!1m3!1e8!2b0!3e3!1m3!1e3!2b1!3e2!1m3!1e9!2b1!3e2!1m3!1e10!2b0!3e3!1m3!1e10!2b1!3e2!1m3!1e10!2b0!3e4!2b1!4b1!9b0!14m5!1s!4m1!2i5361!7e81!12e3!15m21!2b1!5m4!2b1!3b1!5b1!6b1!10m1!8e3!14m1!3b1!17b1!24b1!25b1!26b1!30m1!2b1!36b1!43b1!52b1!56m1!1b1!21m0!22m1!1e81!29m0!30m1!3b1%26q%3DBlue%2BDalia%2BRestaurant%2B%2526%2BTequila%2BBar,%2BWorcester%2BStreet,%2BNatick,%2BMA"
